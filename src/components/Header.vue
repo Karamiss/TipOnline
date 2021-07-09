a7056cf289d0cc0ca3ffc3cc44b15b967daa7474
Fiasko <snakeice2000@mail.ru>
2021-07-09 10:45:01 +0300

    Убрал стандартные данные, добавил шапку

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="navbar-toggle-collapse">
+      <template #default="{ expanded }">
+        <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+        <b-icon v-else icon="chevron-bar-down"></b-icon>
+      </template>
+    </b-navbar-toggle>
+
+    <b-collapse id="navbar-toggle-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item to="/">Home</b-nav-item>
+        <b-nav-item href="#">Link 2</b-nav-item>
+        <b-nav-item to="/about">About</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style  scoped>
+
+</style>
